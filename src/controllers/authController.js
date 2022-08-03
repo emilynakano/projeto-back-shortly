aacb7d37bfbd3990218b268b08318a5c9203a5f1
emily <nakanodepaula@gmail.com>
2022-08-02 23:36:54 -0300

    sign-in ready with send token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,23 +5,44 @@ import {v4 as uuid} from 'uuid'
 
 export async function signUp(req, res) {
     const {name, email, password} = req.body;
-
-    const { rowCount } = await connection.query(`
-    SELECT * FROM users WHERE email=$1`, [email]
-    )
-
-    if(rowCount > 0) {
-        return res.sendStatus(409)
+    try {
+        const { rowCount } = await connection.query(`
+        SELECT * FROM users WHERE email=$1`, [email]
+        )
+    
+        if(rowCount > 0) {
+            return res.sendStatus(409)
+        }
+    
+        const passwordHash = bcrypt.hashSync(password, 10);
+    
+        await connection.query(`
+        INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`, 
+        [name, email, passwordHash] )
+    
+        res.sendStatus(201)
+    } catch {
+        res.sendStatus(500)
     }
-
-    const passwordHash = bcrypt.hashSync(password, 10);
-
-    await connection.query(`
-    INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`, 
-    [name, email, passwordHash] )
-
-    res.sendStatus(201)
 }
 export async function signIn(req, res) {
+    const {email, password} = req.body;
+
+    const {rows: users, rowCount} = await connection.query(`
+    SELECT * FROM users WHERE email=$1`,
+    [email]);
+
+    const user = users[0];
+
+    if(rowCount > 0 && bcrypt.compareSync(password, user.password)) {
+        const token = uuid();
+        
+        await connection.query(`
+        INSERT INTO sessions ("userId", token) VALUES ($1, $2)`, [user.id, token]);
+
+        return res.status(200).send(token)
+    } else {
+        return res.status(401)
+    }
 }
 
