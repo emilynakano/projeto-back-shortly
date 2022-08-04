26088fe58c622b8da8ebc038b425958a76991e1c
emily <nakanodepaula@gmail.com>
2022-08-04 09:18:01 -0300

    get url by id and verification if it exists

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,5 +1,4 @@
 import connection from '../databases/postgres.js'
-import joi from 'joi';
 import { nanoid } from 'nanoid'
 
 export async function createShortUrl(req, res) {
@@ -8,9 +7,31 @@ export async function createShortUrl(req, res) {
 
     const shortUri = nanoid();
 
-    await connection.query(`
-    INSERT INTO "shortenedUrls" ("userId", "shortUrl", "url") VALUES ($1, $2, $3)`,
-    [user[0].id, shortUri, url])
+    try {
+        await connection.query(`
+        INSERT INTO "shortenedUrls" ("userId", "shortUrl", "url") VALUES ($1, $2, $3)`,
+        [user[0].id, shortUri, url])
+    
+        res.send({"shortUri": shortUri}).status(201)
+    } catch {
+        res.sendStatus(500)
+    }
 
-    res.send({"shortUri": shortUri}).status(201)
+}
+
+export async function getUrlById(req, res) {
+    try {
+        const {rows: url, rowCount} = await connection.query(`
+        SELECT id, "shortUrl", url FROM "shortenedUrls" WHERE id=$1`,
+        [req.params.id]);
+    
+        if(rowCount === 0) {
+            return res.sendStatus(404)
+        }
+    
+        res.send(url[0]).status(200)
+    } catch {
+        res.sendStatus(500)
+    }
+
 }
